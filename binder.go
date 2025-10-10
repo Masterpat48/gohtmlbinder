6e1e2785eedb44d027b5a80549977f9ad47fea5a
Masterpat48 <152006028+Masterpat48@users.noreply.github.com>
2025-10-10 18:09:01 +0200

    INIT OF THE REPO

---
+++ binder.go
@@ -0,0 +1,58 @@
+package binder
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+	"path/filepath"
+
+	"github.com/gorilla/mux"
+)
+
+type Binder struct {
+	router    *mux.Router
+	templates *template.Template
+	baseDir   string
+}
+
+// function new used to start the binder
+// baseTempalte can be a file or a pattern like "templates/*html"
+func new(baseTemplate string) *Binder {
+	baseDir := filepath.Dir(baseTemplate)
+	pattern := filepath.Join(baseDir, "*hmtl")
+
+	tmpl := template.Must(template.ParseGlob(pattern))
+
+	return &Binder{
+		router:    mux.NewRouter(),
+		templates: tmpl,
+		baseDir:   baseDir,
+	}
+}
+
+// function NewRoute used to create a route based on a HTML template
+func (b *Binder) NewRoute(path string, templateName string) {
+	b.router.HandleFunc(path, func(w http.ResponseWriter, r *http.Request) {
+		err := b.templates.ExecuteTemplate(w, templateName, nil)
+		if err != nil {
+			http.Error(w, fmt.Sprintf("rendering error %s: %v", templateName, err), http.StatusInternalServerError)
+		}
+	})
+}
+
+// function NewRouteData used to create a route w dynamic data
+func (b *Binder) NewRouteData(path string, templateName string, dataFunc func(*http.Request) any) {
+	b.router.HandleFunc(path, func(w http.ResponseWriter, r *http.Request) {
+		data := dataFunc(r)
+		err := b.templates.ExecuteTemplate(w, templateName, data)
+		if err != nil {
+			http.Error(w, fmt.Sprintf("rendering error %s: %v", templateName, err), http.StatusInternalServerError)
+		}
+	})
+}
+
+// function serve used to start the server with a chosen port
+func (b *Binder) serve(addr string) error {
+	fmt.Printf("Server started on https://localhost%s\n", addr)
+	return http.ListenAndServe(addr, b.router)
+}
